0956edff9a7854b5b0ce2592a60f5a609693480e
Anthony Arias <aarias92@gmail.com>
2025-04-28 10:45:50 -0500

    register function is now working

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:"",
+    })
+
+    const handleChange = e =>{
+        setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleSubmit = async e =>{
+        e.preventDefault()
+        try{
+            const res = await axios.post("/auth/register", inputs)
+            console.log(res);
+        }catch(err){
+            console.log(err)
+        }
+        
+    }
+
     return (
         <div className='auth'>
             <h1>Register</h1>
             <form className='loginForm'>
-                <input className='inputBox' type='text' placeholder='username' required/>
-                <input className='inputBox' type='email' placeholder='email' required/>
-                <input className='inputBox' type='password' placeholder='password' required/>
-                <button className='btn btn-primary'>Register</button>
+                <input className='inputBox' type='text' placeholder='username' required name="username" onChange={handleChange}/>
+                <input className='inputBox' type='email' placeholder='email' required name="email" onChange={handleChange}/>
+                <input className='inputBox' type='password' placeholder='password' required name="password" onChange={handleChange}/>
+                <button className='btn btn-primary' onClick={handleSubmit}>Register</button>
                 <span className="inputFooter">Have an account? <Link to="/login">Login</Link></span>
             </form>
         </div>
