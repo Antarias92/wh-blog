60ad94bfb1e2f95bd90892cf4c5af09ee7360142
Anthony Arias <aarias92@gmail.com>
2025-04-29 13:06:52 -0500

    logout functionality added and username displayed in navbar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { AuthContext } from '../Context/authContext';
 
 const Login = () =>{
 
@@ -10,7 +11,10 @@ const Login = () =>{
         password:"",
     })
     const [err, setError] = useState(null);
+
     const navigate = useNavigate();
+
+    const {login} = useContext(AuthContext);
 
     const handleChange = e =>{
         setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
@@ -19,7 +23,7 @@ const Login = () =>{
     const handleSubmit = async e =>{
         e.preventDefault()
         try{
-            await axios.post("/auth/login", inputs)
+            await login(inputs)
             navigate("/wh-blog")
         }catch(err){
            setError(err.response.data)
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Context/authContext";
 
 const Navbar = ({children}) => {
+    const {currentUser, logout } = useContext(AuthContext);
+
     return (
         <div className="container mt-4 mx-auto bgNav">
            <nav className="navbar navbar-expand-lg bg-dark">
                <div className="container-fluid">
                    <Link style={{color: "#EBB866"}} className="navbar-brand logo" to="/">Warhammer Database</Link>
-                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                    <span className="navbar-toggler-icon bg-white"></span>
                    </button>
                    <div className="collapse navbar-collapse" id="navbarNav">
@@ -19,7 +22,11 @@ const Navbar = ({children}) => {
                                <Link style={{color: "#ebb866"}} className="nav-link" to="/add"> Add Item </Link>
                            </li>
                            <li className="nav-item menu-item">
-                               <Link style={{color: "#ebb866"}} className="nav-link" to="/"> Logout </Link>
+                               <span style={{color: "#ebb866"}} className="nav-link">{currentUser?.username}</span>
+                           </li>
+                           <li className="nav-item menu-item">
+                               <Link style={{color: "#ebb866"}} className="nav-link" to="/login" onClick={logout}> Logout </Link>
+                               {/*{currentUser ? <span style={{color: "#ebb866"}} className="nav-link">Logout</span> : <Link style={{color: "#ebb866"}} className="nav-link" to="/login">Login</Link>}*/}
                            </li>
                        </ul>
                    </div>
